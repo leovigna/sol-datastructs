3793251ed110faca755477c13af6ab66adc33f96
Leo Vigna <leo.vigna@gmail.com>
2021-03-13 16:10:16 +0100

    fix npm ignore files (#5)

--- migrations-ts/2_deploy_contracts.ts
+++
@@ -1,7 +0,0 @@
-//async function (deployer, network, accounts) {}
-module.exports = async function() {
-    console.debug('Deploy here.');
-} as Truffle.Migration;
-
-// because of https://stackoverflow.com/questions/40900791/cannot-redeclare-block-scoped-variable-in-unrelated-files
-export {};
